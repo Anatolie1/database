c8bd23c1f134aac1739cb84b18eb91d13d66a46c
Anatolie1 <58854021+Anatolie1@users.noreply.github.com>
2020-01-30 23:08:28 +0100

    Add files via upload

---
+++ SQLQuery_Quetes3.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE customers;
+USE customers;
+CREATE TABLE Customer_DB (
+id INT PRIMARY KEY IDENTITY(1,1),
+name VARCHAR(100) NOT NULL);
+INSERT INTO Customer_DB (name)
+VALUES ('DelaZ' ),
+('RichZ'),
+('FedasZ');
+SELECT * FROM Customer_DB;
+UPDATE Customer_DB
+SET name = TRIM('Z' FROM name);
+SELECT * FROM Customer_DB;
